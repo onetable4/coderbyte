5be038f0ecc03cdbad4f40ed0ee85668fec4ae34
onetable4 <onetable4@naver.com>
2015-07-29 15:41:00 +0900

    Create ExOh.rb

---
+++ ExOh.rb
@@ -0,0 +1,14 @@
+def ExOh(str)
+
+  # code goes here
+  if str.count("x") == str.count("o")
+    return "true"
+  else
+    return "false"
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ExOh(STDIN.gets)  
