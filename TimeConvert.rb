edc98e08842c988c60dcf009c0012af91287c435
onetable4 <onetable4@naver.com>
2015-07-29 15:05:56 +0900

    Create TimeConvert.rb

---
+++ TimeConvert.rb
@@ -0,0 +1,14 @@
+def TimeConvert(num)
+
+  # code goes here
+  return (num / 60).to_s + ":" + (num % 60).to_s
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)  
+
+
+
+
