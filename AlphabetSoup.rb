81bc45de47c5f4b125565115cb838f0a275948c1
onetable4 <onetable4@naver.com>
2015-07-30 10:44:02 +0900

    Create AlphabetSoup.rb

---
+++ AlphabetSoup.rb
@@ -0,0 +1,14 @@
+def AlphabetSoup(str)
+
+  # code goes here
+  strarray = str.split""
+  strarray.sort!
+  strarray = strarray - [" "]
+  str = strarray.join("")
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)  
